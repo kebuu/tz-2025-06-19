01dbbf1a1f1eac593928023b61d370de74db4096
ChristopheTARDELLA <christophe.tardella@gmail.com>
2025-06-09 17:03:22 +0200

    Use UseCase annotation

--- backend/src/main/kotlin/com/example/rememberme/RememberMeApplication.kt
+++ backend/src/main/kotlin/com/example/rememberme/RememberMeApplication.kt
@@ -1,18 +1,22 @@
 package com.example.rememberme
 
 import com.example.rememberme.shared.domain.annotation.UseCase
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner
 import org.springframework.core.type.filter.AnnotationTypeFilter
 
 @SpringBootApplication
 class RememberMeApplication {
     @Bean
-    fun useCaseComponentScanner(): ClassPathScanningCandidateComponentProvider {
-        return ClassPathScanningCandidateComponentProvider(false)
-            .apply { addIncludeFilter(AnnotationTypeFilter(UseCase::class.java)) }
+    fun useCaseRegistryPostProcessor(): BeanDefinitionRegistryPostProcessor {
+        return BeanDefinitionRegistryPostProcessor { registry ->
+            val scanner = ClassPathBeanDefinitionScanner(registry, false)
+            scanner.addIncludeFilter(AnnotationTypeFilter(UseCase::class.java))
+            scanner.scan("com.example.rememberme.user.domain.usecase")
+        }
     }
 }
 
